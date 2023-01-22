3366873b66453a1258ea0f4a1a18622a376f4f76
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2023-01-22 19:17:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-
-
+class API{
+  constructor(url)
+  this.url = url
+  this.method = "GET"
+  this.#secure = url.startWith("https");
+}
+isSecure() {
+    return this.#secure
+}
+updateURL(url) {
+  this.url = url
+}
+let firstAPI = new API('http://api.com/api/hello')
+console. log(s.isSecure()) // false
+let secondAPI = new API('https://api.com/api/hello')
+console. log(s.isSecure()) // true
+    
 module.exports = { API }
